5e78473165de45c505fd1ab2637feec5002b1e3f
AndrewHany2 <andrewhany260@gmaiil.com>
2023-07-05 02:00:07 +0300

    seed data into db

---
+++ server/helpers/seed.js
@@ -0,0 +1,47 @@
+const { Sender, Biker } = require("../models");
+const dbMongo = require("./connection");
+const { encryptPassword } = require("./encryption");
+
+(async () => {
+  try {
+    const dbCheck = await dbMongo.connect();
+    if (dbCheck) {
+      let senders = [];
+      let bikers = [];
+      for (let i = 1; i <= 5; i++) {
+        try {
+          const newSender = {
+            name: `Sender${i}`,
+            email: `sender${i}@mailinator.com`,
+            password: await encryptPassword(`senderpassword_${i}`),
+            parcels: [],
+          };
+          senders.push(newSender);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      const sendersCheck = await Sender.insertMany(senders);
+      if (sendersCheck) {
+        console.log("===========All senders Inserted==============");
+      }
+      // Create and save 10 bikers
+      for (let i = 1; i <= 10; i++) {
+        const biker = {
+          name: `Biker ${i}`,
+          email: `biker${i}@mailinator.com`,
+          password: await encryptPassword(`bikerpassword_${i}`),
+          parcels: [],
+        };
+        bikers.push(biker);
+      }
+      const bikersCheck = await Biker.insertMany(bikers);
+      if (bikersCheck) {
+        console.log("===========All bikersInserted==============");
+      }
+    }
+  } catch (error) {
+    console.log("error", error);
+    process.exit(1);
+  }
+})();
